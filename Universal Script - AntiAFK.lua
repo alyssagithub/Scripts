cc0bfff298758b656d439ce05223ef53c6227eb7
icee <78248903+alyssagithub@users.noreply.github.com>
2022-08-22 00:08:04 -0700

    Update Universal Script - AntiAFK.lua

--- Universal Script - AntiAFK.lua
+++ Universal Script - AntiAFK.lua
@@ -1,3 +1,5 @@
+-- Feel free to put this in your auto execute folder, as this should not affect any game's gameplay nor be detected.
+
 local VirtualUser = game:GetService("VirtualUser")
 
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
