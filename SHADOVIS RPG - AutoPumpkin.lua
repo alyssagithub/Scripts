91ebe8347f3c871bb8b93861030ba97ca3d147b8
icee <78248903+alyssagithub@users.noreply.github.com>
2022-08-26 01:44:53 -0700

    Update SHADOVIS RPG - AutoPumpkin.lua

--- SHADOVIS RPG - AutoPumpkin.lua
+++ SHADOVIS RPG - AutoPumpkin.lua
@@ -6,6 +6,8 @@ while AutoPumpkin and task.wait(.25) do
     --game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5063, 700, 4765) -- perfect grinding spot for <300k level
     
     --game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5045, 692, 3401) -- perfect grinding spot for <2m level
+    
+    --game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5061, 687, 2909) -- perfect grinding spot for rebirth 6+ (aureus)
   
     local args = {
         [1] = "Input",
