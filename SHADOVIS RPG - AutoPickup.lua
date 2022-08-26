dbcc54b1113bdc80c37b0b66b761d3791c2724b0
icee <78248903+alyssagithub@users.noreply.github.com>
2022-08-26 01:08:47 -0700

    Update SHADOVIS RPG - AutoPickup.lua

--- SHADOVIS RPG - AutoPickup.lua
+++ SHADOVIS RPG - AutoPickup.lua
@@ -1,9 +1,11 @@
 getgenv().AutoPickup = true
 
+local Blacklist = {"ItemName", "ItemName", "ItemName"} -- Replace ItemName with the item's name that you don't want to pick up, to add more slots create a new string and seperate with a comma
+
 while AutoPickup and task.wait() do
 	if #workspace.Projectiles:GetChildren() > 0 then
 		for i,v in pairs(workspace.Projectiles:GetDescendants()) do
-			if v:IsA("ProximityPrompt") then
+			if v:IsA("ProximityPrompt") and not table.find(Blacklist, v.Parent.Name) then
 				fireproximityprompt(v)
 			end
 		end
