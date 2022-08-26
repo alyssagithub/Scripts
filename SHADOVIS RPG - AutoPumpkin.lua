ef75599644f7387c965de83821c84ceaf225bad4
icee <78248903+alyssagithub@users.noreply.github.com>
2022-08-26 01:22:30 -0700

    Create SHADOVIS RPG - AutoPumpkin.lua

---
+++ SHADOVIS RPG - AutoPumpkin.lua
@@ -0,0 +1,18 @@
+getgenv().AutoPumpkin = true
+
+-- only use if you own the Hallow Greatsword
+
+while AutoPumpkin and task.wait(.25) do
+    --game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5063, 700, 4765) -- perfect grinding spot for <300k level
+    
+    --game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5045, 692, 3401) -- perfect grinding spot for <2m level
+  
+    local args = {
+        [1] = "Input",
+        [2] = "HallowHallow Greatsword",
+        [3] = 4.571428571428571,
+        [4] = "Hallows"
+    }
+
+    game:GetService("Players").LocalPlayer.Character.Combat.RemoteEvent:FireServer(unpack(args))
+end
