79e91d6a65eb8ed143f1a5f05575776c9c56f944
icee <78248903+alyssagithub@users.noreply.github.com>
2022-08-27 01:34:07 -0700

    Update Trade Simulator - New Item Buyer.lua

--- Trade Simulator - New Item Buyer.lua
+++ Trade Simulator - New Item Buyer.lua
@@ -1,12 +1,3 @@
 -- This script is in development.
--- So far, all this script will do is detect a new item and open up the item's page, could still be useful for quick buying and/or macro buying.
-
-game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Notification.Container.Title:GetPropertyChangedSignal("Text"):Connect(function()
-	if game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Notification.Container.Title.Text == "New Item for Sale" then
-		game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Notification.Container.Title.Text = ""
-		for i,v in next, getconnections(game:GetService("Players").LocalPlayer.PlayerGui.MainUI.Notification.Container.Button.MouseButton1Click) do
-			v:Fire()
-			-- insert item buyer script here
-		end
-	end
-end)
+-- Step 1: Detect when a new item comes out and open the item's page, Done
+-- Step 2: Buy the item once, then check if the item is actually good based on timer or quantity
