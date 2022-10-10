8fafcfb31f872f409700da9d930b986ad802436b
icee <78248903+alyssagithub@users.noreply.github.com>
2022-10-10 00:33:02 -0700

    Update Script Hub - Inferno X.lua

--- Script Hub - Inferno X.lua
+++ Script Hub - Inferno X.lua
@@ -687,7 +687,7 @@ elseif game.PlaceId == 10925589760 then
 			OriginalAutoMergeLooping = Value
 			while AutoMergeLooping and task.wait() do
 				for i,v in pairs(Plot.Blocks:GetChildren()) do
-					v.CFrame = CFrame.new(427, 9, 97)
+					v.CFrame = CFrame.new(Plot.Main.Position.X + 10, Plot.Main.Position.Y + 10, Plot.Main.Position.Z + 10)
 				end
 			end
 		end
