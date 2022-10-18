87f950b3639b035504cfb9ca90197d014ab4eb2e
icee <78248903+alyssagithub@users.noreply.github.com>
2022-10-17 20:46:25 -0700

    Update Script Hub - Inferno X.lua

--- Script Hub - Inferno X.lua
+++ Script Hub - Inferno X.lua
@@ -1037,7 +1037,7 @@ elseif game.PlaceId == 9625096419 then
 	local Pets = Main:AddSection({
 		Name = "Pets"
 	})
-	
+
 	local Max
 
 	if tonumber(Player.PlayerGui.ScreenGui.Updates.TextLabel.Text:split(".")[3]) >= 6 then
@@ -1192,6 +1192,41 @@ elseif game.PlaceId == 9625096419 then
 			end
 		end
 	})
+	
+	local function comma(amount)
+		local formatted = amount
+		local k
+		while true do  
+			formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+			if (k==0) then
+				break
+			end
+		end
+		return formatted
+	end
+	
+	Misc:AddButton({
+		Name = "🔢 Check Rebirth Amount",
+		Callback = function()
+			Abbreviation.Abbreviate = function(e, number)
+				return tostring(number)
+			end
+			
+			Click(Player.PlayerGui.ScreenGui.Clickers.Tapper)
+			
+			repeat task.wait() until tonumber(Player.PlayerGui.ScreenGui.Currencies.Currency1.Amount.Text) and tonumber(Player.PlayerGui.ScreenGui.Currencies.Currency1.Amount.Text) >= 800
+			
+			RebirthLabel:Set("Rebirth Amount: "..comma(math.floor(tonumber(Player.PlayerGui.ScreenGui.Currencies.Currency1.Amount.Text) / tonumber(Player.PlayerGui.ScreenGui.Menus.Rebirths.Menu.Holder["1"].Cost.Text:split(" ")[1]))))
+			
+			task.wait()
+			
+			Abbreviation.Abbreviate = PreviousFunction
+			
+			Click(Player.PlayerGui.ScreenGui.Clickers.Tapper)
+		end    
+	})
+	
+	RebirthLabel = Misc:AddLabel("Rebirth Amount: nil")
 
 	Credits(Window)
 end
