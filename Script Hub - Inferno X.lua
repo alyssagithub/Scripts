a978ddef85cf5e76ecb5a5acdc798fa027cab281
icee <78248903+alyssagithub@users.noreply.github.com>
2022-10-26 15:19:12 -0700

    Update Script Hub - Inferno X.lua

--- Script Hub - Inferno X.lua
+++ Script Hub - Inferno X.lua
@@ -359,14 +359,12 @@ if game.PlaceId == 9264596435 then -- Idle Heroes Simulator
 						SavedPosition = Player.Character:WaitForChild("HumanoidRootPart").Position
 
 						task.wait()
-
-						Player.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(-1886, 61, -92)
-
-						task.wait(.5)
-
-						game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.HeroService.RE.HireHero:FireServer(game:GetService("Workspace").Main.Hire["_displayHero"].Head.Nametag.Subject.Text)
-
-						task.wait(.5)
+						
+						repeat
+							Player.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(-1886, 61, -92)
+							task.wait()
+							game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.HeroService.RE.HireHero:FireServer(game:GetService("Workspace").Main.Hire["_displayHero"].Head.Nametag.Subject.Text)
+						until tostring(game:GetService("Workspace").Main.Hire["_displayHero"].Highlight.OutlineColor) ~= "0.215686, 1, 0.266667" or not HireLooping
 
 						Player.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(SavedPosition)
 
