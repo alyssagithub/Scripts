8681dce40b1d03131c1f9891f1ec758864274e19
icee <78248903+alyssagithub@users.noreply.github.com>
2022-11-21 00:21:48 -0800

    Add files via upload

---
+++ Inferno X Scripts/AutoRejoin.lua
@@ -0,0 +1,12 @@
+repeat task.wait() until game.CoreGui:FindFirstChild('RobloxPromptGui')
+ 
+local lp,po,ts = game:GetService('Players').LocalPlayer,game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService('TeleportService')
+ 
+po.ChildAdded:connect(function(a)
+    if a.Name == 'ErrorPrompt' then
+        while true do
+            ts:Teleport(game.PlaceId)
+            task.wait(2)
+        end
+    end
+end)
