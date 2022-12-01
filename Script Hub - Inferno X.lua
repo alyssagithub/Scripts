4cbae94f435da5795b54d6e4c68350a4f8b531a0
icee <78248903+alyssagithub@users.noreply.github.com>
2022-12-01 10:30:47 -0800

    Update Script Hub - Inferno X.lua

--- Script Hub - Inferno X.lua
+++ Script Hub - Inferno X.lua
@@ -3,7 +3,6 @@ local Games = {
 	[10925589760] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Merge%20Simulator.lua", -- Merge Simulator
 	[9712123877] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Super%20Slime%20Simulator.lua", -- Super Slime Simulator
 	[11189979930] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Pet%20Crafting%20Simulator.lua", -- Pet Crafting Simulator
-	[11102985540] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Swarm%20Simulator.lua", -- Swarm Simulator
 	[10404327868] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Timber%20Champions.lua", -- Timber Champions
 	[10594623896] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Master%20Punching%20Simulator.lua" -- Master Punching Simulator
 }
