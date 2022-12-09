e2fe74f562b5e0f0615b503509121dc03103fcff
icee <78248903+alyssagithub@users.noreply.github.com>
2022-12-08 19:31:40 -0800

    Update One Fruit Simulator.lua

--- Inferno X Scripts/Games/One Fruit Simulator.lua
+++ Inferno X Scripts/Games/One Fruit Simulator.lua
@@ -1,6 +1,6 @@
 local Player, Rayfield, Click, comma, Notify, CreateWindow, CurrentVersion = loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Variables.lua"))()
 
-CurrentVersion("v1.1.0")
+CurrentVersion("v1.1.1")
 
 local Islands = {"None"}
 local Quests = {}
