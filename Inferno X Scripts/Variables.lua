1341859b6933b0e2b211ab84909b021db00af00f
icee <78248903+alyssagithub@users.noreply.github.com>
2022-12-19 16:18:10 -0800

    Update Variables.lua

--- Inferno X Scripts/Variables.lua
+++ Inferno X Scripts/Variables.lua
@@ -287,6 +287,83 @@ local function CreateWindow()
 				end)
 			end
 		end)
+		
+		Universal:CreateButton({
+			Name = "🔂 One-Time Server Hop",
+			Callback = function()
+				local Http = game:GetService("HttpService")
+				local TPS = game:GetService("TeleportService")
+				local Api = "https://games.roblox.com/v1/games/"
+
+				local _place,_id = game.PlaceId, game.JobId
+				local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
+
+				local function ListServers(cursor)
+					local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+					return Http:JSONDecode(Raw)
+				end
+
+				local Next; repeat
+					local Servers = ListServers(Next)
+					for i,v in next, Servers.data do
+						if v.playing < v.maxPlayers and v.id ~= _id then
+							local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+							if s then break end
+						end
+					end
+
+					Next = Servers.nextPageCursor
+				until not Next
+			end,
+		})
+		
+		Universal:CreateToggle({
+			Name = "🔁 Server Hop",
+			Info = "Automatically server hops after the interval",
+			CurrentValue = false,
+			Flag = "Universal-ServerHop",
+			Callback = function(Value)	end,
+		})
+		
+		Universal:CreateSlider({
+			Name = "⏲ Server Hop Intervals",
+			Info = "Sets the interval in seconds for the Server Hop",
+			Range = {5, 600},
+			Increment = 1,
+			CurrentValue = 5,
+			Flag = "Universal-ServerhopIntervals",
+			Callback = function(Value)	end,
+		})
+		
+		task.spawn(function()
+			while task.wait(Rayfield.Flags["Universal-ServerhopIntervals"].CurrentValue) do
+				if Rayfield.Flags["Universal-ServerHop"].CurrentValue then
+					local Http = game:GetService("HttpService")
+					local TPS = game:GetService("TeleportService")
+					local Api = "https://games.roblox.com/v1/games/"
+
+					local _place,_id = game.PlaceId, game.JobId
+					local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
+					
+					local function ListServers(cursor)
+						local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+						return Http:JSONDecode(Raw)
+					end
+
+					local Next; repeat
+						local Servers = ListServers(Next)
+						for i,v in next, Servers.data do
+							if v.playing < v.maxPlayers and v.id ~= _id then
+								local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+								if s then break end
+							end
+						end
+
+						Next = Servers.nextPageCursor
+					until not Next
+				end
+			end
+		end)
 
 		local Credits = Window:CreateTab("Credits/Suggestions", 4483362458)
 
