43a676259ba4f15fbf83266bea0a8db86816415b
icee <78248903+alyssagithub@users.noreply.github.com>
2023-03-20 14:46:30 -0700

    Update Script Hub - Inferno X.lua

--- Script Hub - Inferno X.lua
+++ Script Hub - Inferno X.lua
@@ -3,7 +3,8 @@ local Games = {
 	[10925589760] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Merge%20Simulator.lua", -- Merge Simulator
 	[10404327868] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Timber%20Champions.lua", -- Timber Champions
 	[11445923563] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/One%20Fruit%20Simulator.lua", -- One Fruit Simulator
-	[10821317529] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
+	[10821317529] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
+	[11040063484] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Sword%20Fighters%20Simulator.lua" -- Sword Fighters Simulator
 }
 
 if Games[game.PlaceId] then
