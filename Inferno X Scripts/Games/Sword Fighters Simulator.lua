227c254b4cc62d173b456d2850defc38acf4e765
icee <78248903+alyssagithub@users.noreply.github.com>
2023-03-20 16:36:15 -0700

    Update Sword Fighters Simulator.lua

--- Inferno X Scripts/Games/Sword Fighters Simulator.lua
+++ Inferno X Scripts/Games/Sword Fighters Simulator.lua
@@ -1,12 +1,15 @@
-local loadstring, pairs, table_find, table_insert, task_spawn, task_wait, tick, Vector3_new, tostring = loadstring, pairs, table.find, table.insert, task.spawn, task.wait, tick, Vector3.new, tostring
-
-local Player, Rayfield, Click, comma, Notify, CreateWindow, CurrentVersion = loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Variables.lua"))()
-
-CurrentVersion("v1")
+local Player, Rayfield, Click, comma, Notify, CreateWindow = loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Variables.lua"))()
 
 local Services = game:GetService("ReplicatedStorage").Packages.Knit.Services
-local workspace = workspace
 local virtualInput = game:GetService("VirtualInputManager")
+
+local Up10 = Vector3.new(0, 10, 0)
+local huge = math.huge
+local workspace = workspace
+local tick = tick
+local task = task
+local pairs = pairs
+local table = table
 
 local Quests = {}
 local Eggs = {}
@@ -15,49 +18,49 @@ local Portals = {"None"}
 local NPCs = {"Closest NPC"}
 
 local function ClosestEgg(v)
-    local Number = math.huge
-    local Egg
-    
-    for e,r in pairs(workspace.Live.FloatingEggs:GetChildren()) do
-        local Magnitude = (v.Position - r.PrimaryPart.Position).Magnitude
-        if Magnitude < Number then
-            Number = Magnitude
-            Egg = r
-        end
-    end
-    
-    EggsTP[Egg.Name] = v
-    
-    return Egg
+	local Number = huge
+	local Egg
+
+	for e,r in pairs(workspace.Live.FloatingEggs:GetChildren()) do
+		local Magnitude = (v.Position - r.PrimaryPart.Position).Magnitude
+		if Magnitude < Number then
+			Number = Magnitude
+			Egg = r
+		end
+	end
+
+	EggsTP[Egg.Name] = v
+
+	return Egg
 end
 
 for i,v in pairs(Player.PlayerGui.Index.Background.ImageFrame.Window.Frames.WeaponFrame.WeaponHolder.WeaponScrolling:GetChildren()) do
-    if v:IsA("Frame") then
-        table_insert(NPCs, v.Name)
-    end
+	if v:IsA("Frame") then
+		table.insert(NPCs, v.Name)
+	end
 end
 
 for i,v in pairs(workspace.Resources.Eggs:GetChildren()) do
-    ClosestEgg(v)
+	ClosestEgg(v)
 end
 
 for i,v in pairs(workspace.Resources.QuestDummy:GetChildren()) do
-	table_insert(Quests, v.Name)
+	table.insert(Quests, v.Name)
 end
 
 for i,v in pairs(workspace.Live.FloatingEggs:GetChildren()) do
-    if not table_find(Eggs, v.Name) then
-        table_insert(Eggs, v.Name)
-    end
+	if not table.find(Eggs, v.Name) then
+		table.insert(Eggs, v.Name)
+	end
 end
 
 for i,v in pairs(Player.PlayerGui:GetChildren()) do
 	if v.Name == "Portal" then
-		table_insert(Portals, v.Text1.Text)
+		table.insert(Portals, v.Text1.Text)
 	end
 end
 
-local Window = CreateWindow()
+local Window = CreateWindow("v1")
 
 local Main = Window:CreateTab("Main", 4483362458)
 
@@ -68,23 +71,23 @@ Main:CreateToggle({
 	Info = "Automatically gains power & attacks the closest NPC",
 	CurrentValue = false,
 	Flag = "Swing",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
-task_spawn(function()
-	while task_wait() do
+task.spawn(function()
+	while task.wait() do
 		if Rayfield.Flags.Swing.CurrentValue then
 			local Mob
-			local Number = math.huge
-			
+			local Number = huge
+
 			for i,v in pairs(workspace.Live.NPCs.Client:GetChildren()) do
-			    local Magnitude = (Player.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
+				local Magnitude = (Player.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
 				if Magnitude < Number then
 					Mob = v.Name
 					Number = Magnitude
 				end
 			end
-			
+
 			Services.ClickService.RF.Click:InvokeServer(Mob)
 		end
 	end
@@ -93,39 +96,39 @@ end)
 Main:CreateDropdown({
 	Name = "🏅 NPC",
 	Options = NPCs,
-	CurrentOption = "Goblin",
+	CurrentOption = "",
 	Flag = "NPC",
-	Callback = function(Option)	end,
+	Callback = function()end,
 })
 
 Main:CreateToggle({
 	Name = "🗡 Teleport to NPC",
 	CurrentValue = false,
 	Flag = "TPNPC",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
-task_spawn(function()
-    while task_wait() do
-        if Rayfield.Flags.TPNPC.CurrentValue then
-            local Number = math.huge
-            local ClosestNPC
-            
-            for i,v in pairs(workspace.Live.NPCs.Client:GetChildren()) do
-                if v and v:FindFirstChild("HumanoidRootPart") and v.HumanoidRootPart:FindFirstChild("NPCTag") and (Rayfield.Flags.NPC.CurrentOption == v.HumanoidRootPart.NPCTag.NameLabel.Text or Rayfield.Flags.NPC.CurrentOption == "Closest NPC") and (Player.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude < Number then
-                    Number = (Player.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
-                    ClosestNPC = v
-                end
-            end
-            
-            if ClosestNPC then
-                repeat
-                    Player.Character.HumanoidRootPart.CFrame = ClosestNPC.HumanoidRootPart.CFrame + Vector3_new(0, 10, 0) + ClosestNPC.HumanoidRootPart.CFrame.LookVector * 5
-                    task_wait()
-                until not ClosestNPC or not workspace.Live.NPCs.Client:FindFirstChild(ClosestNPC.Name) or not Rayfield.Flags.TPNPC.CurrentValue
-            end
-        end
-    end
+task.spawn(function()
+	while task.wait() do
+		if Rayfield.Flags.TPNPC.CurrentValue then
+			local Number = huge
+			local ClosestNPC
+
+			for i,v in pairs(workspace.Live.NPCs.Client:GetChildren()) do
+				if v and v:FindFirstChild("HumanoidRootPart") and v.HumanoidRootPart:FindFirstChild("NPCTag") and (Rayfield.Flags.NPC.CurrentOption == v.HumanoidRootPart.NPCTag.NameLabel.Text or Rayfield.Flags.NPC.CurrentOption == "Closest NPC") and (Player.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude < Number then
+					Number = (Player.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
+					ClosestNPC = v
+				end
+			end
+
+			if ClosestNPC then
+				repeat
+					Player.Character.HumanoidRootPart.CFrame = ClosestNPC.HumanoidRootPart.CFrame + Up10 + ClosestNPC.HumanoidRootPart.CFrame.LookVector * 5
+					task.wait()
+				until not ClosestNPC or not workspace.Live.NPCs.Client:FindFirstChild(ClosestNPC.Name) or not Rayfield.Flags.TPNPC.CurrentValue
+			end
+		end
+	end
 end)
 
 Main:CreateSection("Farming")
@@ -134,11 +137,11 @@ Main:CreateToggle({
 	Name = "💎 Auto Collect Drops",
 	CurrentValue = false,
 	Flag = "Drops",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
-task_spawn(function()
-	while task_wait() do
+task.spawn(function()
+	while task.wait() do
 		if Rayfield.Flags.Drops.CurrentValue then
 			for i,v in pairs(workspace.Live.Pickups:GetChildren()) do
 				if v then
@@ -153,11 +156,11 @@ Main:CreateToggle({
 	Name = "💸 Auto Ascend",
 	CurrentValue = false,
 	Flag = "Ascend",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
-task_spawn(function()
-	while task_wait() do
+task.spawn(function()
+	while task.wait() do
 		if Rayfield.Flags.Ascend.CurrentValue and Player.PlayerGui.LeftSidebar.Background.Frame.BottomButtons.Ascend.ReadyLabel.Visible then
 			Services.AscendService.RF.Ascend:InvokeServer()
 		end
@@ -169,16 +172,16 @@ Main:CreateToggle({
 	Info = "Automatically starts & completes quests.",
 	CurrentValue = false,
 	Flag = "AutoQuest",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
-task_spawn(function()
-	while task_wait() do
+task.spawn(function()
+	while task.wait() do
 		if Rayfield.Flags.AutoQuest.CurrentValue then
-		    for i,v in pairs(Quests) do
-		        Services.QuestService.RF.ActionQuest:InvokeServer(v)
-		    end
-		    task_wait(1)
+			for i,v in pairs(Quests) do
+				Services.QuestService.RF.ActionQuest:InvokeServer(v)
+			end
+			task.wait(1)
 		end
 	end
 end)
@@ -188,14 +191,14 @@ Main:CreateSection("Hatching")
 local EggDropdown = Main:CreateDropdown({
 	Name = "🥚 Egg",
 	Options = Eggs,
-	CurrentOption = "Weak Egg",
+	CurrentOption = "",
 	Flag = "Egg",
-	Callback = function(Option)	end,
+	Callback = function()end,
 })
 
 workspace.Resources.Eggs.ChildAdded:Connect(function(Child)
-    local Egg = ClosestEgg(Child)
-    
+	local Egg = ClosestEgg(Child)
+
 	EggDropdown:Add(Egg.Name)
 end)
 
@@ -203,11 +206,11 @@ Main:CreateToggle({
 	Name = "🐣 Auto Hatch",
 	CurrentValue = false,
 	Flag = "Hatch",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
-task_spawn(function()
-	while task_wait() do
+task.spawn(function()
+	while task.wait() do
 		if Rayfield.Flags.Hatch.CurrentValue and EggsTP[Rayfield.Flags.Egg.CurrentOption] then
 			Player.Character.HumanoidRootPart.CFrame = EggsTP[Rayfield.Flags.Egg.CurrentOption].CFrame
 			Services.EggService.RF.BuyEgg:InvokeServer({["eggName"] = EggsTP[Rayfield.Flags.Egg.CurrentOption].Name, ["auto"] = false, ["amount"] = 1})
@@ -221,12 +224,12 @@ Main:CreateToggle({
 	Name = "🔪 Equip Best",
 	CurrentValue = false,
 	Flag = "EquipBest",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
 Player.PlayerGui.PetInv.Background.ImageFrame.Window.PetHolder.PetScrolling.ChildAdded:Connect(function()
-    if Rayfield.Flags.EquipBest.CurrentValue then
-        Services.PetInvService.RF.EquipBest:InvokeServer()
+	if Rayfield.Flags.EquipBest.CurrentValue then
+		Services.PetInvService.RF.EquipBest:InvokeServer()
 	end
 end)
 
@@ -235,7 +238,7 @@ Main:CreateToggle({
 	Info = "Forge your weapons into divine/godly!",
 	CurrentValue = false,
 	Flag = "Forge",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
 Main:CreateToggle({
@@ -243,25 +246,25 @@ Main:CreateToggle({
 	Info = "Automatically adds stars to your godly weapons!",
 	CurrentValue = false,
 	Flag = "Star",
-	Callback = function(Value)	end,
+	Callback = function()end,
 })
 
 Player.PlayerGui.WeaponInv.Background.ImageFrame.Window.WeaponHolder.WeaponScrolling.ChildAdded:Connect(function(Child)
-    if Rayfield.Flags.EquipBest.CurrentValue then
-        Services.WeaponInvService.RF.EquipBest:InvokeServer()
-    end
+	if Rayfield.Flags.EquipBest.CurrentValue then
+		Services.WeaponInvService.RF.EquipBest:InvokeServer()
+	end
 
-    if Rayfield.Flags.Forge.CurrentValue then
-        Services.ForgeService.RF.Forge:InvokeServer(Child.Name)
-    end
-    
-    if Rayfield.Flags.Star.CurrentValue then
-        for i,v in pairs(Player.PlayerGui.WeaponInv.Background.ImageFrame.Window.WeaponHolder.WeaponScrolling:GetChildren()) do
-            if v:IsA("Frame") and v:FindFirstChild("Frame") and v.Frame.Tier.Visible and tostring(v.Frame.Tier.BackgroundColor3) == "0.988235, 0.843137, 0.141176" then
-                Services.WeaponStarService.RF.AddStar:InvokeServer(v.Name)
-            end
-        end
-    end
+	if Rayfield.Flags.Forge.CurrentValue then
+		Services.ForgeService.RF.Forge:InvokeServer(Child.Name)
+	end
+
+	if Rayfield.Flags.Star.CurrentValue then
+		for i,v in pairs(Player.PlayerGui.WeaponInv.Background.ImageFrame.Window.WeaponHolder.WeaponScrolling:GetChildren()) do
+			if v:IsA("Frame") and v:FindFirstChild("Frame") and v.Frame.Tier.Visible and tostring(v.Frame.Tier.BackgroundColor3) == "0.988235, 0.843137, 0.141176" then
+				Services.WeaponStarService.RF.AddStar:InvokeServer(v.Name)
+			end
+		end
+	end
 end)
 
 Main:CreateSection("Transportation")
@@ -276,11 +279,11 @@ Main:CreateDropdown({
 			if v.Name == "Portal" and v.Text1.Text == Option then
 				Player.Character.HumanoidRootPart.CFrame = v.Adornee.CFrame
 				local Start = tick()
-		
-		        repeat
-		            virtualInput:SendKeyEvent(true, "E", false, nil)
-		            task_wait()
-		        until Player.PlayerGui.Transition.Background.Visible == true or Start - tick() >= 1
+
+				repeat
+					virtualInput:SendKeyEvent(true, "E", false, nil)
+					task.wait()
+				until Player.PlayerGui.Transition.Background.Visible == true or tick() - Start > 1
 				break
 			end
 		end
