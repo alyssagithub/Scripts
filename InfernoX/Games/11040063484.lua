50a2a1ba58f728806ffaa5ee928e5774e4ec26dc
icee <78248903+alyssagithub@users.noreply.github.com>
2023-03-31 22:50:07 -0700

    Update and rename Inferno X Scripts/Games/Sword Fighters Simulator.lua to InfernoX/Games/11040063484.lua

--- InfernoX/Games/11040063484.lua
+++ InfernoX/Games/11040063484.lua
@@ -1,4 +1,4 @@
-local Player, Rayfield, Click, comma, Notify, CreateWindow = loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Variables.lua"))()
+local Player, Rayfield, Click, comma, Notify, CreateWindow = loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/InfernoX/Variables.lua"))()
 
 local Services = game:GetService("ReplicatedStorage").Packages.Knit.Services
 local virtualInput = game:GetService("VirtualInputManager")
