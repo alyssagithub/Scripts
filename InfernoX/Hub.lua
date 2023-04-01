c7663fb85ad7ebb73b23c37835d9211e98cee51b
icee <78248903+alyssagithub@users.noreply.github.com>
2023-03-31 22:53:56 -0700

    Update Hub.lua

--- InfernoX/Hub.lua
+++ InfernoX/Hub.lua
@@ -1,11 +1,11 @@
 local StarterGui = game:GetService("StarterGui")
 local link = game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/InfernoX/Games/"..game.PlaceId..".lua")
 
-if Games[game.PlaceId] then
+if Link then
   StarterGui:SetCore("SendNotification", {
 		Title = "Inferno X Notification",
 		Text = "Welcome. Loading script.",
 		Duration = 10
 	})
-	loadstring(game:HttpGet(Games[game.PlaceId]))()
+	loadstring(Link)()
 end
