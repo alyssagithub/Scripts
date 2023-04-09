155c23f3fb9f003b0b25a5dd81a4360c8eb55ee6
icee <78248903+alyssagithub@users.noreply.github.com>
2023-04-09 02:40:09 -0700

    Anime Warriors Simulator 2

--- InfernoX/Games/8425128940.lua
+++ InfernoX/Games/8425128940.lua
@@ -4,7 +4,7 @@ local workspace = workspace
 local huge = math.huge
 
 local RemoteEvent = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent")
-local Services = game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services
+local Services = game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.4.7"):WaitForChild("knit"):WaitForChild("Services")
 
 RemoteEvent:FireServer({{"!", "EnemyRender", 500}})
 
