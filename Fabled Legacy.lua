0c2bfdc9449f76e63b5c6df544ce37c76fa546a2
fraud <78248903+alyssagithub@users.noreply.github.com>
2023-04-16 17:22:48 -0700

    Update Fabled Legacy.lua

--- Fabled Legacy.lua
+++ Fabled Legacy.lua
@@ -4,36 +4,33 @@ end
 
 pcall(queue_on_teleport or syn.queue_on_teleport or fluxus.queue_on_teleport, 'loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/main/Fabled%20Legacy.lua"))()')
 
-repeat task.wait() until game:FindFirstChild("ReplicatedStorage")
+pcall(function()
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local UseSpell = ReplicatedStorage:FindFirstChild("useSpell")
+	local Swing = ReplicatedStorage.Swing
+	local HumanoidRootPart = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+	_G.Enabled = true
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local UseSpell = ReplicatedStorage:WaitForChild("useSpell", 3)
-if not UseSpell then
-	return
-end
-local Swing = ReplicatedStorage.Swing
-local HumanoidRootPart = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-_G.Enabled = true
+	while _G.Enabled and task.wait() do
+		local Number = math.huge
+		local Closest
 
-while _G.Enabled and task.wait() do
-	local Number = math.huge
-	local Closest
+		for i,v in pairs(workspace.Enemies:GetChildren()) do
+			if v and v:FindFirstChild("HumanoidRootPart") and not workspace:FindFirstChild("SpellPart4") then
+				local Magnitude = (HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
 
-	for i,v in pairs(workspace.Enemies:GetChildren()) do
-		if v and v:FindFirstChild("HumanoidRootPart") and not workspace:FindFirstChild("SpellPart4") then
-			local Magnitude = (HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
-
-			if Magnitude <= 15 and Magnitude < Number then
-				Number = Magnitude
-				Closest = v.HumanoidRootPart
+				if Magnitude <= 15 and Magnitude < Number then
+					Number = Magnitude
+					Closest = v.HumanoidRootPart
+				end
 			end
 		end
-	end
 
-	if Closest then
-		HumanoidRootPart.CFrame = CFrame.lookAt(HumanoidRootPart.Position, Vector3.new(Closest.Position.X, HumanoidRootPart.Position.Y, Closest.Position.Z))
-		UseSpell:FireServer("Q")
-		UseSpell:FireServer("E")
-		Swing:FireServer()
+		if Closest then
+			HumanoidRootPart.CFrame = CFrame.lookAt(HumanoidRootPart.Position, Vector3.new(Closest.Position.X, HumanoidRootPart.Position.Y, Closest.Position.Z))
+			UseSpell:FireServer("Q")
+			UseSpell:FireServer("E")
+			Swing:FireServer()
+		end
 	end
-end
+end)
