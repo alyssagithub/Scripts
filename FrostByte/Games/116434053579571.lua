730b5ad3bb6ad35c742358ab3ac9fde77f5f0977
fraud <78248903+alyssagithub@users.noreply.github.com>
2024-12-29 14:05:03 -0800

    Mining GO!

--- FrostByte/Games/116434053579571.lua
+++ FrostByte/Games/116434053579571.lua
@@ -1,4 +1,4 @@
-local Version = "v1.2.1"
+local Version = "v1.3.1"
 
 local MarketplaceService = game:GetService("MarketplaceService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -12,6 +12,7 @@ local BinderEvent: RemoteEvent = ReplicatedStorage._Binder_Event
 local BinderFunction: RemoteFunction = ReplicatedStorage._Binder_Function
 
 local UpgradeTreeImages = ReplicatedStorage.Assets.UpgradeTreeImages
+local InteractZones = ReplicatedStorage.InteractZones
 
 local GiantOreSummary = Player.PlayerGui.GameGui.GiantOreSummary
 
@@ -26,6 +27,25 @@ local TierValues = {
 	S = 4,
 	X = 5
 }
+
+local Dispensers = {}
+
+for i,v in InteractZones:GetChildren() do
+	if v.Name:lower():find("dispenser") then
+		table.insert(Dispensers, v.Name)
+	end
+end
+
+local function CollectDrops(Enabled: boolean)
+	if not Enabled then
+		return
+	end
+
+	for i,v in workspace.Drops:GetChildren() do
+		firetouchinterest(v.Frame, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+		firetouchinterest(v.Frame, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+	end
+end
 
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
 
@@ -56,10 +76,10 @@ local Window = Rayfield:CreateWindow({
 
 local Tab = Window:CreateTab("Automation", "repeat")
 
-Tab:CreateSection("Mining")
+Tab:CreateSection("Ores")
 
 Tab:CreateToggle({
-	Name = "⛏ Auto Mine",
+	Name = "⛏ • Auto Mine",
 	CurrentValue = false,
 	Flag = "Mine",
 	Callback = function(Value)
@@ -99,17 +119,30 @@ Tab:CreateToggle({
 })
 
 Tab:CreateToggle({
-	Name = "💥 Auto Critical Strike Ores (Fails Without a Crit Zone)",
+	Name = "💥 • Auto Critical Strike Ores (Needs a Crit Zone)",
 	CurrentValue = false,
 	Flag = "Critical",
 	Callback = function(Value)
 	end,
 })
 
-Tab:CreateDivider()
+Tab:CreateToggle({
+	Name = "💎 • Auto Collect Drops",
+	CurrentValue = false,
+	Flag = "Collect",
+	Callback = CollectDrops,
+})
+
+if CollectConnection then
+	CollectConnection:Disconnect()
+end
+
+getgenv().CollectConnection = workspace.Drops.ChildAdded:Connect(function()
+	CollectDrops(Flags.Collect.CurrentValue)
+end)
 
 Tab:CreateToggle({
-	Name = "🧱 Auto TP to Ores",
+	Name = "🧱 • Auto TP to Ores (for Ore Pulse)",
 	CurrentValue = false,
 	Flag = "TP",
 	Callback = function(Value)
@@ -140,36 +173,10 @@ Tab:CreateToggle({
 	end,
 })
 
-local function CollectDrops(Enabled: boolean)
-	if not Enabled then
-		return
-	end
-	
-	for i,v in workspace.Drops:GetChildren() do
-		firetouchinterest(v.Frame, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
-		firetouchinterest(v.Frame, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
-	end
-end
+Tab:CreateSection("Marketplace")
 
 Tab:CreateToggle({
-	Name = "💎 Auto Collect Drops",
-	CurrentValue = false,
-	Flag = "Collect",
-	Callback = CollectDrops,
-})
-
-if CollectConnection then
-	CollectConnection:Disconnect()
-end
-
-getgenv().CollectConnection = workspace.Drops.ChildAdded:Connect(function()
-	CollectDrops(Flags.Collect.CurrentValue)
-end)
-
-Tab:CreateSection("Upgrades")
-
-Tab:CreateToggle({
-	Name = "💰 Auto Purchase from Ore Marketplace",
+	Name = "💰 • Auto Purchase from Ore Marketplace",
 	CurrentValue = false,
 	Flag = "Marketplace",
 	Callback = function(Value)
@@ -183,6 +190,10 @@ Tab:CreateToggle({
 
 				ElementNumber += 1
 
+				if Flags.Free.CurrentValue and v.Inner.Cost.Title.Text ~= "0" then
+					continue
+				end
+
 				BinderFunction:InvokeServer("Marketplace_Purchase", tostring(ElementNumber))
 			end
 		end
@@ -190,11 +201,29 @@ Tab:CreateToggle({
 })
 
 Tab:CreateToggle({
-	Name = "🔁 Auto Refresh Ore Marketplace",
+	Name = "💹 • Only Purchase Free Listings",
+	CurrentValue = false,
+	Flag = "Free",
+	Callback = function()end,
+})
+
+Tab:CreateToggle({
+	Name = "🔁 • Auto Refresh Ore Marketplace",
 	CurrentValue = false,
 	Flag = "Refresh",
 	Callback = function(Value)
 		while Flags.Refresh.CurrentValue and task.wait(1) do
+			local Title = Player.PlayerGui.GameGui.OreMarketplace.Refresh.RefreshButton.Cost.Title
+			local Numbers = Title.Text:gsub("%D", "")
+
+			if Title:find("k") then
+				Numbers ..= "000"
+			end
+
+			if tonumber(Numbers) > Flags.MaxRefresh.CurrentValue then
+				continue
+			end
+
 			local ElementNumber = 0
 			local SuccessfulPurchases = 0
 
@@ -208,20 +237,53 @@ Tab:CreateToggle({
 				if Deal.Inner.Center.Purchased2.Visible then
 					SuccessfulPurchases += 1
 				end
+			end
 
-				if SuccessfulPurchases == ElementNumber then
-					BinderFunction:InvokeServer("Marketplace_Refresh")
-					task.wait(5)
-				end
+			if SuccessfulPurchases == ElementNumber then
+				BinderFunction:InvokeServer("Marketplace_Refresh")
+				task.wait(5)
 			end
 		end
 	end,
 })
 
-Tab:CreateDivider()
+Tab:CreateSlider({
+	Name = "💵 • Max Refresh Price",
+	Range = {5, 10000},
+	Increment = 5,
+	Suffix = "Ores",
+	CurrentValue = 5,
+	Flag = "MaxRefresh",
+	Callback = function()end,
+})
+
+Tab:CreateSection("Dispensers")
 
 Tab:CreateToggle({
-	Name = "📈 Auto Upgrade",
+	Name = "🔄 • Auto Roll Equipment",
+	CurrentValue = false,
+	Flag = "Roll",
+	Callback = function(Value)
+		while Flags.Roll.CurrentValue and task.wait() do
+			BinderFunction:InvokeServer("Roll_RollItem", Flags.Dispenser.CurrentOption[1])
+			CollectDrops(true)
+		end
+	end,
+})
+
+Tab:CreateDropdown({
+	Name = "⚒ • Equipment Dispenser",
+	Options = Dispensers,
+	CurrentOption = Dispensers[1],
+	MultipleOptions = false,
+	Flag = "Dispenser",
+	Callback = function()end,
+})
+
+Tab:CreateSection("Upgrades")
+
+Tab:CreateToggle({
+	Name = "📈 • Auto Upgrade",
 	CurrentValue = false,
 	Flag = "Upgrade",
 	Callback = function(Value)
@@ -234,7 +296,7 @@ Tab:CreateToggle({
 				task.spawn(function()
 					if BinderFunction:InvokeServer("Tree_Upgrade", Upgrade.Name) then
 						BoughtUpgrades[Upgrade.Name] = true
-						
+
 						Rayfield:Notify({
 							Title = Upgrade.Name,
 							Content = "Bought by Auto Upgrade!",
@@ -249,7 +311,7 @@ Tab:CreateToggle({
 })
 
 Tab:CreateToggle({
-	Name = "📜 Auto Quests",
+	Name = "📜 • Auto Quests",
 	CurrentValue = false,
 	Flag = "Quests",
 	Callback = function(Value)
@@ -261,28 +323,28 @@ Tab:CreateToggle({
 			if QuestElem and not QuestElem.Inner.TextArea.Title.Text:lower():find("complete") then
 				local CompletedTasks = 0
 				local TotalTasks = 0
-				
+
 				for _, Task: TextLabel in QuestElem.Inner.TextArea:GetChildren() do
 					if not Task:IsA("TextLabel") then
 						continue
 					end
-					
+
 					TotalTasks += 1
-					
+
 					if Task.FontFace.Bold then
 						CompletedTasks += 1
 					end
 				end
-				
+
 				if CompletedTasks ~= TotalTasks then
 					continue
 				end
 			end
 
 			QuestTPing = true
-			
+
 			local MouseHumanoidRootPart = workspace.Map.Mouse.HumanoidRootPart
-			
+
 			if (Player.Character:GetPivot().Position - MouseHumanoidRootPart.Position).Magnitude > 10 then
 				Player.Character:PivotTo(MouseHumanoidRootPart:GetPivot())
 			end
@@ -294,7 +356,9 @@ Tab:CreateToggle({
 	end,
 })
 
-Tab:CreateSection("QOL")
+local Tab = Window:CreateTab("QOL", "leaf")
+
+Tab:CreateSection("UI")
 
 local UIStroke = Instance.new("UIStroke")
 UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
@@ -302,66 +366,66 @@ UIStroke.Color = Color3.fromRGB(255, 255, 255)
 UIStroke.Thickness = 5
 
 Tab:CreateToggle({
-	Name = "📦 Highlight Best Keep/Replace Choice (Based on Tier)",
+	Name = "📦 • Highlight Best Keep/Replace Choice (Based on Tier)",
 	CurrentValue = false,
 	Flag = "KeepReplace",
 	Callback = function(Value)
 		while Flags.KeepReplace.CurrentValue and task.wait() do
 			local Roll = Player.PlayerGui.StartGui.Roll
-			
+
 			if not Roll.Visible or not Roll.Old.Visible then
 				continue
 			end
-			
+
 			local NewStats = Roll.New.Stat.Frame
 			local OldStats = Roll.Old.Stat.Frame
-			
+
 			local Combined = {
 				[NewStats] = 0,
 				[OldStats] = 0
 			}
-			
+
 			for _, Stats in {NewStats, OldStats} do
 				for _, Stat in Stats:GetChildren() do
 					if Stat.Name ~= "Stat" then
 						continue
 					end
-					
+
 					Combined[Stats] += TierValues[Stat.Tier.Text]
 				end
 			end
-			
+
 			if Combined[NewStats] == Combined[OldStats] then
 				return
 			end
-			
+
 			local NewButton = Roll.New.Button
 			local OldButton = Roll.Old.Button
-			
+
 			local NewUIStroke = NewButton:FindFirstChild("UIStroke")
 			local OldUIStroke = OldButton:FindFirstChild("UIStroke")
-			
+
 			if NewUIStroke and NewUIStroke ~= UIStroke then
 				NewUIStroke.Parent = nil
 			end
-			
+
 			if OldUIStroke and OldUIStroke ~= UIStroke then
 				OldUIStroke.Parent = nil
 			end
-			
+
 			if Combined[NewStats] > Combined[OldStats] then
 				UIStroke.Parent = NewButton
 			else
 				UIStroke.Parent = OldButton
 			end
 		end
-		
+
 		UIStroke.Parent = nil
 	end,
 })
 
 Tab:CreateToggle({
-	Name = "❌ Remove Giant Ore Summary",
+	Name = "❌ • Remove Giant Ore Summary",
 	CurrentValue = false,
 	Flag = "GiantSummary",
 	Callback = function(Value)
@@ -379,12 +443,61 @@ getgenv().SummaryConnection = GiantOreSummary:GetPropertyChangedSignal("Visible"
 	end
 end)
 
+Tab:CreateSection("Safety")
+
+local Part
+
+local function GenerateRandomPos(Min, Max, Negative)
+	local Positive = Random.new():NextNumber(Min, Max)
+	
+	if not Negative then
+		return Positive
+	end
+	
+	return if math.random(2) == 1 then Random.new():NextNumber(Min, Max) else Random.new():NextNumber(-Min, -Max)
+end
+
+Tab:CreateToggle({
+	Name = "🔑 • Teleport to Safe Location",
+	CurrentValue = false,
+	Flag = "Safe",
+	Callback = function(Value)
+		if Value and not Part then
+			Part = Instance.new("Part")
+			Part.Anchored = true
+			Part.Position = Vector3.new(GenerateRandomPos(800, 1000, true), GenerateRandomPos(800, 1000), GenerateRandomPos(800, 1000, true))
+			Part.Size = Vector3.new(10, 2, 10)
+			Part.Parent = workspace
+		end
+		
+		local PreviousLocation = Player.Character and Player.Character:GetPivot()
+		
+		while Flags.Safe.CurrentValue and task.wait() do
+			local Character = Player.Character
+			
+			if not Character or (Character:GetPivot().Position - Part.Position).Magnitude < 15 then
+				continue
+			end
+			
+			if QuestTPing then
+				continue
+			end
+			
+			Character:PivotTo(Part:GetPivot() + Vector3.yAxis * 5)
+		end
+		
+		if Value and not Flags.Safe.CurrentValue then
+			Player.Character:PivotTo(PreviousLocation)
+		end
+	end,
+})
+
 local Tab = Window:CreateTab("Universal", "earth")
 
 Tab:CreateSection("AFK")
 
 Tab:CreateToggle({
-	Name = "🔒 Anti AFK Disconnection",
+	Name = "🔒 • Anti AFK Disconnection",
 	CurrentValue = true,
 	Flag = "AntiAFK",
 	Callback = function(Value)
@@ -399,7 +512,7 @@ getgenv().IdledConnection = Player.Idled:Connect(function()
 	if not Flags.AntiAFK.CurrentValue then
 		return
 	end
-	
+
 	VirtualUser:CaptureController()
 	VirtualUser:ClickButton2(Vector2.zero)
 end)
