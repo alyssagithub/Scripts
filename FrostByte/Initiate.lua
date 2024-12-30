e85d3714b64cf3cdd7259429ee39cc76c2935366
fraud <78248903+alyssagithub@users.noreply.github.com>
2024-12-29 21:12:03 -0800

    Update Initiate.lua

--- FrostByte/Initiate.lua
+++ FrostByte/Initiate.lua
@@ -2,10 +2,10 @@
 local Code = game:HttpGet(`https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Games/{game.PlaceId}.lua`)
 
 if Code then
-	loadstring(Code)()
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Title = "FrostByte Notification",
 		Text = "Game found, the script is loading.",
 		Duration = 10
 	})
+	loadstring(Code)()
 end
