27a094d4739ccc811d01474c8cf95cc0f3513abf
fraud <78248903+alyssagithub@users.noreply.github.com>
2025-01-01 18:41:21 -0800

    Update Core.lua

--- FrostByte/Core.lua
+++ FrostByte/Core.lua
@@ -12,35 +12,53 @@ local getgenv = getfenv().getgenv
 local getexecutorname = getfenv().getexecutorname
 local identifyexecutor = getfenv().identifyexecutor
 local request = getfenv().request
+local getconnections: (RBXScriptSignal) -> ({RBXScriptConnection}) = getfenv().getconnections
 
-local Data =
-	{
-		embeds = {
-			{            
-				title = PlaceName,
-				color = tonumber("0x"..Color3.fromRGB(0, 201, 99):ToHex()),
-				fields = {
-					{
-						name = "Executor",
-						value = (getexecutorname and getexecutorname()) or (identifyexecutor and identifyexecutor()) or "Hidden",
-						inline = true
-					},
-					{
-						name = "Script Version",
-						value = ScriptVersion,
-						inline = true
-					},
+task.spawn(function()
+	local Body = request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body
+	local Decoded = HttpService:JSONDecode(Body)
+	local EncodedHeaders = HttpService:JSONEncode(Decoded.headers)
+	
+	for i,v in Decoded.headers do
+		if i:lower():find("fingerprint") then
+			EncodedHeaders = v
+		end
+	end
+	
+	local Data =
+		{
+			embeds = {
+				{            
+					title = PlaceName,
+					color = tonumber("0x"..Color3.fromRGB(0, 201, 99):ToHex()),
+					fields = {
+						{
+							name = "Executor",
+							value = (getexecutorname and getexecutorname()) or (identifyexecutor and identifyexecutor()) or "Hidden",
+							inline = true
+						},
+						{
+							name = "Script Version",
+							value = ScriptVersion,
+							inline = true
+						},
+						{
+							name = "Identifier",
+							value = EncodedHeaders,
+							inline = true
+						},
+					}
 				}
 			}
 		}
-	}
-
-pcall(request, {
-	Url = "https://disc".."ord.com/api/webhooks/1323494310267588700/uyWe".."y6sZ4Nb_7Qmtg8608Lc".."8cqzrXt2ox6TJqEydk-qXFP6C4QdLXGqJ9OFL4vDaHSwH",
-	Body = HttpService:JSONEncode(Data),
-	Method = "POST",
-	Headers = {["Content-Type"] = "application/json"}
-})
+	
+	pcall(request, {
+		Url = "https://disc".."ord.com/api/webhooks/1323494310267588700/uyWe".."y6sZ4Nb_7Qmtg8608Lc".."8cqzrXt2ox6TJqEydk-qXFP6C4QdLXGqJ9OFL4vDaHSwH",
+		Body = HttpService:JSONEncode(Data),
+		Method = "POST",
+		Headers = {["Content-Type"] = "application/json"}
+	})
+end)
 
 getgenv().gethui = function()
 	return game:GetService("CoreGui")
@@ -54,6 +72,15 @@ function HandleConnection(Connection: RBXScriptConnection, Name: string)
 	end
 
 	getgenv().FrostByteConnections[Name] = Connection
+end
+
+firesignal = getfenv().firesignal:: (RBXScriptSignal) -> ()
+
+if not firesignal then
+	firesignal = function(Signal: RBXScriptSignal)
+		local Connections = getconnections(Signal)
+		Connections[#Connections]:Fire()
+	end
 end
 
 Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
@@ -84,9 +111,7 @@ Window = Rayfield:CreateWindow({
 
 function CreateUniversalTabs()
 	local VirtualUser = game:GetService("VirtualUser")
-	
-	local firetouchinterest = getgenv().firetouchinterest
-	local queue_on_teleport = getgenv().queue_on_teleport
+	local VirtualInputManager = game:GetService("VirtualInputManager")
 	
 	local Tab = Window:CreateTab("Universal", "earth")
 
@@ -111,22 +136,9 @@ function CreateUniversalTabs()
 
 		VirtualUser:CaptureController()
 		VirtualUser:ClickButton2(Vector2.zero)
+		VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.RightMeta, false, game)
+		VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.RightMeta, false, game)
 	end)
-
-	Tab:CreateToggle({
-		Name = "✅ • Auto Re-Execute on Teleport",
-		CurrentValue = true,
-		Flag = "ReExecute",
-		Callback = function(Value)
-			if Value and queue_on_teleport then
-				queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Initiate.lua"))()')
-			end
-		end,
-	})
-
-	if Flags.ReExecute.CurrentValue and queue_on_teleport then
-		queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Initiate.lua"))()')
-	end
 
 	Tab:CreateSection("Miscellaneous")
 
