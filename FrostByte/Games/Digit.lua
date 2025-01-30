632ab12a29839d11aefbda0636af82156223779a
fraud <78248903+alyssagithub@users.noreply.github.com>
2025-01-30 01:34:28 -0800

    Update Digit.lua

--- FrostByte/Games/Digit.lua
+++ FrostByte/Games/Digit.lua
@@ -1,6 +1,6 @@
 local getgenv: () -> ({[string]: any}) = getfenv().getgenv
 
-getgenv().ScriptVersion = "v2.5.1a"
+getgenv().ScriptVersion = "v2.5.1b"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Core.lua"))()
 
