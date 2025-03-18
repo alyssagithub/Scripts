d806c60ab292a059eb6527329c42eb65923ed19b
fraud <78248903+alyssagithub@users.noreply.github.com>
2025-03-18 02:03:58 -0700

    Update Core.lua

--- FrostByte/Core.lua
+++ FrostByte/Core.lua
@@ -154,16 +154,6 @@ task.spawn(function()
 	end
 end)
 
-local VirtualUser = game:GetService("VirtualUser")
-local VirtualInputManager = game:GetService("VirtualInputManager")
-
-HandleConnection(Player.Idled:Connect(function()
-	VirtualUser:CaptureController()
-	VirtualUser:ClickButton2(Vector2.zero)
-	VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.RightMeta, false, game)
-	VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.RightMeta, false, game)
-end), "AntiAFK")
-
 local OriginalFlags = {}
 
 if getgenv().Flags then
@@ -250,7 +240,7 @@ local Flags: Flags = Rayfield.Flags
 getgenv().Flags = Flags
 
 local Window = Rayfield:CreateWindow({
-	Name = `FrostByte | {PlaceName} | {ScriptVersion or "Dev Mode"}`,
+	Name = `<font color="rgb(120,238,245)">Frost</font><font color="rgb(255,255,255)">Byte</font> | {PlaceName} | {ScriptVersion or "Dev Mode"}`,
 	Icon = "snowflake",
 	LoadingTitle = "❄ Brought to you by FrostByte ❄",
 	LoadingSubtitle = PlaceName,
@@ -280,6 +270,10 @@ Tab:CreateSection("Join our Discord!")
 
 Tab:CreateLabel("discord.gg/sS3tDP6FSB", "messages-square")
 
+Tab:CreateSection("Changelog")
+
+Tab:CreateParagraph({Title = `{PlaceName} {ScriptVersion}`, Content = getgenv().Changelog or "Changelog Not Found"})
+
 Tab:CreateSection("Performance")
 
 local PingLabel = Tab:CreateLabel("Ping: 0 ms", "wifi")
@@ -294,9 +288,45 @@ task.spawn(function()
 	end
 end)
 
-Tab:CreateSection("Changelog")
+Tab:CreateDivider()
 
-Tab:CreateParagraph({Title = `{PlaceName} {ScriptVersion}`, Content = getgenv().Changelog or "Changelog Not Found"})
+local VirtualUser = game:GetService("VirtualUser")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+
+local LastProtected
+
+HandleConnection(Player.Idled:Connect(function()
+	VirtualUser:CaptureController()
+	VirtualUser:ClickButton2(Vector2.zero)
+	VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.RightMeta, false, game)
+	VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.RightMeta, false, game)
+
+	LastProtected = tick()
+end), "AntiAFK")
+
+local AFKMessage = "Anti AFK Protected You:"
+
+local AntiAFKLabel = Tab:CreateLabel(`{AFKMessage} Never`, "shield")
+
+task.spawn(function()
+	while getgenv().Flags == Flags and task.wait(1) do
+		if not LastProtected then
+			continue
+		end
+		
+		AntiAFKLabel:Set(`{AFKMessage} {math.floor(tick() - LastProtected)} seconds ago`)
+	end
+end)
+
+local PlaytimeMessage = "Time Spent In Session:"
+
+local PlaytimeLabel = Tab:CreateLabel(`{PlaytimeMessage} 0 seconds`, "timer")
+
+task.spawn(function()
+	while getgenv().Flags == Flags and task.wait(1) do
+		PlaytimeLabel:Set(`{PlaytimeMessage} {math.floor(tick() - StartLoadTime)} seconds`)
+	end
+end)
 
 --------------------------------------------------------------------------------------------------------------
 
@@ -497,183 +527,6 @@ if FrostByteStarted then
 end
 
 --[[function CreateUniversalTabs()
-	local VirtualUser = game:GetService("VirtualUser")
-	local VirtualInputManager = game:GetService("VirtualInputManager")
-	local TeleportService = game:GetService("TeleportService")
-	local RunService = game:GetService("RunService")
-
-	local Tab: Tab = Window:CreateTab("Client", "user")
-
-	Tab:CreateSection("Discord")
-
-	Tab:CreateButton({
-		Name = "❄ • Join the FrostByte Discord!",
-		Callback = function()
-			if request then
-				request({
-					Url = 'http://127.0.0.1:6463/rpc?v=1',
-					Method = 'POST',
-					Headers = {
-						['Content-Type'] = 'application/json',
-						Origin = 'https://discord.com'
-					},
-					Body = HttpService:JSONEncode({
-						cmd = 'INVITE_BROWSER',
-						nonce = HttpService:GenerateGUID(false),
-						args = {code = 'sS3tDP6FSB'}
-					})
-				})
-			elseif setclipboard then
-				setclipboard("https://discord.gg/sS3tDP6FSB")
-				Notify("Success!", "Copied Discord Link to Clipboard.")
-			end
-
-			Notify("Discord", "https://discord.gg/sS3tDP6FSB")
-		end,
-	})
-
-	Tab:CreateLabel("https://discord.gg/sS3tDP6FSB", "link")
-
-	Tab:CreateSection("Statistics")
-
-	local PingLabel = Tab:CreateLabel("Ping: 0 ms", "wifi")
-	local FPSLabel = Tab:CreateLabel("FPS: 0/s", "monitor")
-
-	local Stats = game:GetService("Stats")
-
-	task.spawn(function()
-		while getgenv().Flags == Flags and task.wait(0.25) do
-			pcall(PingLabel.Set, PingLabel, `Ping: {math.floor(Stats.PerformanceStats.Ping:GetValue() * 100)/ 100} ms`)
-			pcall(FPSLabel.Set, FPSLabel, `FPS: {math.floor(1 / Stats.FrameTime * 10) / 10}/s`)
-		end
-	end)
-
-	Tab:CreateSection("AFK")
-
-	Tab:CreateToggle({
-		Name = "🔒 • Anti AFK Disconnection",
-		CurrentValue = true,
-		Flag = "AntiAFK",
-		Callback = function()end,
-	})
-
-	getgenv().HandleConnection(Player.Idled:Connect(function()
-		if not Flags.AntiAFK.CurrentValue then
-			return
-		end
-
-		VirtualUser:CaptureController()
-		VirtualUser:ClickButton2(Vector2.zero)
-		VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.RightMeta, false, game)
-		VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.RightMeta, false, game)
-	end), "AntiAFK")
-
-	Tab:CreateSection("Performance")
-
-	Tab:CreateSlider({
-		Name = ApplyUnsupportedName("🎮 • Max FPS (0 for Unlimited)", setfpscap),
-		Range = {0, 240},
-		Increment = 1,
-		Suffix = "FPS",
-		CurrentValue = 0,
-		Flag = "MaxFPS",
-		Callback = function(Value)
-			if not setfpscap then
-				return
-			end
-
-			setfpscap(Value)
-		end,
-	})
-
-	local PreviousValue
-
-	Tab:CreateToggle({
-		Name = ApplyUnsupportedName("⬜ • Disable 3D Rendering When Tabbed Out", isrbxactive),
-		CurrentValue = false,
-		Flag = "Rendering",
-		Callback = function(Value)
-			while Flags.Rendering.CurrentValue and task.wait() do
-				local CurrentValue = isrbxactive()
-
-				if PreviousValue == CurrentValue then
-					continue
-				end
-
-				PreviousValue = CurrentValue
-
-				RunService:Set3dRenderingEnabled(CurrentValue)
-			end
-
-			if Value then
-				RunService:Set3dRenderingEnabled(true)
-			end
-		end,
-	})
-
-	Tab:CreateSection("Properties")
-
-	local WalkSpeedConnection: RBXScriptConnection
-	local ConnectedHumanoid
-
-	local function SetWalkSpeed()
-		local Character = Player.Character
-
-		if not Character then
-			return
-		end
-
-		local Humanoid: Humanoid = Character:FindFirstChild("Humanoid")
-
-		if not Humanoid then
-			return
-		end
-
-		if Flags.WalkSpeedChanger.CurrentValue then
-			Humanoid.WalkSpeed = Flags.WalkSpeed.CurrentValue
-		end
-
-		if not WalkSpeedConnection then
-			WalkSpeedConnection = Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(SetWalkSpeed)
-			ConnectedHumanoid = Humanoid
-			HandleConnection(WalkSpeedConnection, "WalkSpeedConnection")
-		end
-	end
-
-	HandleConnection(Player.CharacterAdded:Connect(function()
-		if WalkSpeedConnection then
-			WalkSpeedConnection:Disconnect()
-			WalkSpeedConnection = nil
-		end
-
-		SetWalkSpeed()
-	end), "WalkSpeedCharacterAdded")
-
-	Tab:CreateToggle({
-		Name = "⚡ • Enable WalkSpeed Changer",
-		CurrentValue = false,
-		Flag = "WalkSpeedChanger",
-		Callback = function(Value)
-			if Player.Character and Value then
-				SetWalkSpeed()
-			end
-		end,
-	})
-
-	Tab:CreateSlider({
-		Name = "💨 • Set WalkSpeed",
-		Range = {0, 300},
-		Increment = 1,
-		Suffix = "Studs/s",
-		CurrentValue = game:GetService("StarterPlayer").CharacterWalkSpeed,
-		Flag = "WalkSpeed",
-		Callback = function(Value)
-			SetWalkSpeed()
-		end,
-	})
-
-	Tab:CreateSection("Safety")
-
 	local StaffRoleNames = {
 		"mod",
 		"dev",
@@ -760,79 +613,6 @@ end
 
 	HandleConnection(Players.PlayerAdded:Connect(CheckIfStaff), "StaffJoin")
 
-	getgenv().Role = GetStaffRole(Player)
-
-	Tab:CreateDivider()
-
-	local Connections = {}
-
-	local OriginalText = {}
-
-	local function HandleUsernameChange(Object: Instance)
-		if not Flags.HideName.CurrentValue then
-			return
-		end
-
-		if not Object:IsA("TextLabel") and not Object:IsA("TextBox") and not Object:IsA("TextButton") then
-			return
-		end
-
-		local NameReplacement = Flags.NameReplacement.CurrentValue
-
-		if not Connections[Object] then
-			Connections[Object] = Object:GetPropertyChangedSignal("Text"):Connect(function()
-				HandleUsernameChange(Object)
-			end)
-		end
-
-		if Object.Text:find(Player.Name) then
-			OriginalText[Object] = Object.Text
-			Object.Text = Object.Text:gsub(Player.Name, NameReplacement)
-		elseif Object.Text:find(Player.DisplayName) then
-			OriginalText[Object] = Object.Text
-			Object.Text = Object.Text:gsub(Player.DisplayName, NameReplacement)
-		end
-	end
-
-	local DescendantAddedConnection
-
-	Tab:CreateToggle({
-		Name = "🛡 • Hide Username and Display Name (Client-Sided)",
-		CurrentValue = false,
-		Flag = "HideName",
-		Callback = function(Value)
-			if Value and not DescendantAddedConnection then
-				for i,v in game:GetDescendants() do
-					HandleUsernameChange(v)
-				end
-
-				DescendantAddedConnection = game.DescendantAdded:Connect(HandleUsernameChange)
-
-				HandleConnection(DescendantAddedConnection, "HideName")
-			elseif DescendantAddedConnection then
-				DescendantAddedConnection:Disconnect()
-				DescendantAddedConnection = nil
-
-				for Object: TextLabel?, Text in OriginalText do
-					Object.Text = Text
-				end
-
-				OriginalText = {}
-			end
-		end,
-	})
-
-	Tab:CreateInput({
-		Name = "💬 • Name To Replace With",
-		CurrentValue = "FrostByte",
-		PlaceholderText = "New Name Here",
-		RemoveTextAfterFocusLost = false,
-		Flag = "NameReplacement",
-		Callback = function()end,
-	})
-
-	Tab:CreateDivider()
-
 	Tab:CreateToggle({
 		Name = "🌀 • Noclip",
 		CurrentValue = false,
@@ -856,197 +636,6 @@ end
 					Part.CanCollide = Part:GetAttribute("OriginalCollide") or Part.CanCollide
 				end
 			end
-		end,
-	})
-
-	Tab:CreateSection("Visualizing")
-
-	local CoreGui: Folder = game:GetService("CoreGui")
-
-	local function ESP(TargetPlayer: Player)
-		local TargetCharacter = TargetPlayer.Character or TargetPlayer.CharacterAdded:Wait()
-
-		local LocalCharacter = Player.Character
-
-		local FolderName = `{TargetPlayer.Name}_ESP`
-
-		local Holder = Instance.new("Folder")
-		Holder.Name = FolderName
-		Holder.Parent = CoreGui
-
-		for _, Object: BasePart | Model in TargetCharacter:GetChildren() do
-			if not Object:IsA("PVInstance") then
-				continue
-			end
-
-			local BoxHandleAdornment = Instance.new("BoxHandleAdornment")
-			BoxHandleAdornment.Name = TargetPlayer.Name
-			BoxHandleAdornment.Adornee = Object
-			BoxHandleAdornment.AlwaysOnTop = true
-			BoxHandleAdornment.ZIndex = 10
-			BoxHandleAdornment.Size = if Object:IsA("BasePart") then Object.Size else Object:GetExtentsSize()
-			BoxHandleAdornment.Transparency = 0.5
-			BoxHandleAdornment.Color = BrickColor.White()
-			BoxHandleAdornment.Parent = Holder
-		end
-
-		local BillboardGui = Instance.new("BillboardGui")
-		BillboardGui.Name = TargetPlayer.Name
-		BillboardGui.Adornee = TargetCharacter:FindFirstChild("Head") or TargetCharacter:FindFirstChildWhichIsA("PVInstance")
-		BillboardGui.Size = UDim2.new(0, 100, 0, 150)
-		BillboardGui.StudsOffset = Vector3.new(0, 1, 0)
-		BillboardGui.AlwaysOnTop = true
-		BillboardGui.Parent = Holder
-
-		local TextLabel = Instance.new("TextLabel")
-		TextLabel.BackgroundTransparency = 1
-		TextLabel.Position = UDim2.new(0, 0, 0, -50)
-		TextLabel.Size = UDim2.new(0, 100, 0, 100)
-		TextLabel.Font = Enum.Font.SourceSansSemibold
-		TextLabel.TextSize = 20
-		TextLabel.TextColor3 = Color3.new(1, 1, 1)
-		TextLabel.TextStrokeTransparency = 0
-		TextLabel.TextYAlignment = Enum.TextYAlignment.Bottom
-		TextLabel.Text = "Unloaded"
-		TextLabel.ZIndex = 10
-		TextLabel.Parent = BillboardGui
-
-		TargetPlayer.CharacterAdded:Once(function()
-			if not Flags.ESP.CurrentValue or not Holder.Parent then
-				return
-			end
-
-			if Holder.Parent then
-				Holder:Destroy()
-			end
-
-			ESP(Player)
-		end)
-
-		TargetPlayer.CharacterRemoving:Once(function()
-			Holder:Destroy()
-		end)
-
-		local RenderSteppedConnection: RBXScriptConnection
-		RenderSteppedConnection = RunService.RenderStepped:Connect(function()
-			if not Flags.ESP.CurrentValue then
-				RenderSteppedConnection:Disconnect()
-				return
-			end
-
-			if not Holder.Parent then
-				RenderSteppedConnection:Disconnect()
-				return
-			end
-
-			if not TargetCharacter or not TargetCharacter:FindFirstChildOfClass("Humanoid") or not LocalCharacter or not LocalCharacter:FindFirstChild("Humanoid") then
-				return
-			end
-
-			local Distance = math.floor((LocalCharacter:GetPivot().Position - TargetCharacter:GetPivot().Position).Magnitude)
-			TextLabel.Text = `Name: {TargetPlayer.Name} | Health: {math.floor(TargetCharacter.Humanoid.Health)} | Studs: {Distance}`
-		end)
-	end
-
-	Tab:CreateToggle({
-		Name = "🔍 • Infinite Yield ESP",
-		CurrentValue = false,
-		Flag = "ESP",
-		Callback = function(Value)
-			for _, Object: Folder? in CoreGui:GetChildren() do
-				if not Object.Name:find("_ESP") then
-					continue
-				end
-
-				Object:Destroy()
-			end
-
-			if not Value then
-				return
-			end
-
-			for _, TargetPlayer in Players:GetPlayers() do
-				if TargetPlayer == Player then
-					continue
-				end
-
-				ESP(TargetPlayer)
-			end
-		end,
-	})
-	
-	HandleConnection(Players.PlayerAdded:Connect(ESP), "ESPPlayerAdded")
-
-	Tab:CreateSection("UI")
-
-	local CustomThemes = {
-		BlackHistoryMonth = {
-			TextColor = Color3.fromRGB(),
-
-			Background = Color3.fromRGB(),
-			Topbar = Color3.fromRGB(),
-			Shadow = Color3.fromRGB(),
-
-			NotificationBackground = Color3.fromRGB(),
-			NotificationActionsBackground = Color3.fromRGB(),
-
-			TabBackground = Color3.fromRGB(),
-			TabStroke = Color3.fromRGB(),
-			TabBackgroundSelected = Color3.fromRGB(),
-			TabTextColor = Color3.fromRGB(),
-			SelectedTabTextColor = Color3.fromRGB(),
-
-			ElementBackground = Color3.fromRGB(),
-			ElementBackgroundHover = Color3.fromRGB(),
-			SecondaryElementBackground = Color3.fromRGB(),
-			ElementStroke = Color3.fromRGB(),
-			SecondaryElementStroke = Color3.fromRGB(),
-
-			SliderBackground = Color3.fromRGB(),
-			SliderProgress = Color3.fromRGB(),
-			SliderStroke = Color3.fromRGB(),
-
-			ToggleBackground = Color3.fromRGB(),
-			ToggleEnabled = Color3.fromRGB(),
-			ToggleDisabled = Color3.fromRGB(),
-			ToggleEnabledStroke = Color3.fromRGB(),
-			ToggleDisabledStroke = Color3.fromRGB(),
-			ToggleEnabledOuterStroke = Color3.fromRGB(),
-			ToggleDisabledOuterStroke = Color3.fromRGB(),
-
-			DropdownSelected = Color3.fromRGB(),
-			DropdownUnselected = Color3.fromRGB(),
-
-			InputBackground = Color3.fromRGB(),
-			InputStroke = Color3.fromRGB(),
-			PlaceholderColor = Color3.fromRGB()
-		},
-		Default = "DarkBlue",
-		Dark = "Default"
-	}
-
-	Tab:CreateDropdown({
-		Name = "🖼 • Change Theme",
-		Options = {"BlackHistoryMonth", "Default", "Dark", "AmberGlow", "Amethyst", "Ocean", "Light", "Bloom", "Green", "Serenity"},
-		MultipleOptions = false,
-		Flag = "Theme",
-		Callback = function(CurrentOption)
-			CurrentOption = CurrentOption[1]
-
-			if CurrentOption == "" then
-				return
-			end
-
-			Window.ModifyTheme(CustomThemes[CurrentOption] or CurrentOption)
-		end,
-	})
-
-	Tab:CreateSection("Development")
-
-	Tab:CreateButton({
-		Name = "⚙️ • Rejoin",
-		Callback = function()
-			TeleportService:Teleport(game.PlaceId, Player, {FrostByteRejoin = true})
 		end,
 	})
 end]]
