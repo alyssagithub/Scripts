41f767dbb38b311cf57b0b7a9d0db8b40ccb7db4
fraud <78248903+alyssagithub@users.noreply.github.com>
2025-04-15 10:24:50 -0700

    Update Initiate.lua

--- FrostByte/Initiate.lua
+++ FrostByte/Initiate.lua
@@ -8,23 +8,36 @@ local function Notify(Text)
 	})
 end
 
-local PlaceName = game:GetService("AssetService"):GetGamePlacesAsync(game.GameId):GetCurrentPage()[1].Name
+local function FormatPlaceName(PlaceName: string)
+	getgenv().PlaceName = PlaceName
 
-getgenv().PlaceName = PlaceName
+	PlaceName = PlaceName:gsub("%b[]", "")
+	PlaceName = PlaceName:gsub("[^%a]", "")
+	
+	return PlaceName
+end
 
-PlaceName = PlaceName:gsub("%b[]", "")
-PlaceName = PlaceName:gsub("[^%a]", "")
+local RanPlaces = false
+
+local function GetCode(PlaceName)
+	local Success, Code: string = pcall(game.HttpGet, game, `https://github.com/alyssagithub/Scripts/raw/main/FrostByte/Games/{PlaceName}.luau`)
+
+	if Success and Code:find("ScriptVersion = ") then
+		Notify("Game found, the script is loading.")
+		getgenv().PlaceFileName = PlaceName
+	elseif not RanPlaces then
+		RanPlaces = true
+		return GetCode(FormatPlaceName(game:GetService("AssetService"):GetGamePlacesAsync(game.GameId):GetCurrentPage()[1].Name))
+	else
+		Notify("Game not found, loading universal.")
+		Code = game:HttpGet("https://github.com/alyssagithub/Scripts/raw/main/FrostByte/Games/Universal.luau")
+	end
+	
+	return Code
+end
 
 loadstring(game:HttpGet("https://github.com/alyssagithub/Scripts/raw/main/FrostByte/Analytics.luau"))()
 
-local Success, Code: string = pcall(game.HttpGet, game, `https://github.com/alyssagithub/Scripts/raw/main/FrostByte/Games/{PlaceName}.luau`)
-
-if Success and Code:find("ScriptVersion = ") then
-	Notify("Game found, the script is loading.")
-	getgenv().PlaceFileName = PlaceName
-else
-	Notify("Game not found, loading universal.")
-	Code = game:HttpGet("https://github.com/alyssagithub/Scripts/raw/main/FrostByte/Games/Universal.luau")
-end
+local Code = GetCode(FormatPlaceName(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name))
 
 getgenv().FrostByteHandleFunction(loadstring(Code), "Game")
